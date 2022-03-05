6d0abacd788ac50d66fdec2c0f5929485f2a538b
john-s21 <bijudonbosco1995@gmail.com>
2022-03-05 13:21:43 +0530

    irregular format code added

--- provider.tf
+++ provider.tf
@@ -1,13 +1,13 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source =         "hashicorp/aws"
       version = "3.72.0"
     }
   }
 }
 
 provider "aws" {
-  profile = "default"
+  profile           = "default"
   region  = "us-east-2"
 }
