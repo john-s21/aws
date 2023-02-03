7faa9c3be54d8476c16a39b0b2959239d8d137b2
john-s21 <bijudonbosco1995@gmail.com>
2023-02-03 20:45:41 +0530

    tf sample format

--- master.tf
+++ master.tf
@@ -1,16 +1,16 @@
 resource "aws_instance" "Jenkins-Master" {
-  ami           = lookup(var.ec2_ami, var.region)
+  ami           = ami-06984ea821ac0a879 #lookup(var.ec2_ami, var.region)
   count         = 1
-  instance_type = var.instance_type
-  vpc_security_group_ids = ["sg-a8414ce0"]
+  instance_type = t2.micro #var.instance_type
+  vpc_security_group_ids = ["sg-23f2375c0"]
   # subnet_id              =          "subnet-4eb39d02"
-  key_name   = lookup(var.keyname, var.region)
-  monitoring = true
-  user_data              = file("D:/Terraform/Scripts/ubuntu.sh")
+  key_name   = "southed.pem" #lookup(var.keyname, var.region)
+  #monitoring = true
+  #user_data              = file("D:/Terraform/Scripts/ubuntu.sh")
 
   tags = {
     Terraform   = "True"
     Environment = "DeV"
-    Name        = "SVR01PJTALPHA-A1530-${count.index}"
+    Name        = "test_serv" #"SVR01PJTALPHA-A1530-${count.index}"
   }
 }
