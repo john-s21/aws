20bd2f794d3e34dbfae3faff56c14fcd7cb5c063
JohnPaul <bijudonbosco1995@gmail.com>
2025-05-19 18:58:09 +0530

    Update on master.tf

--- master.tf
+++ master.tf
@@ -1,16 +1,16 @@
-resource "aws_instance" "Jenkins-Master" {
-  ami           = ami-06984ea821ac0a879 #lookup(var.ec2_ami, var.region)
+resource "aws_instance" "TESTSERV001" {
+  ami           = ami-0e35ddab05955cf57 #lookup(var.ec2_ami, var.region)
   count         = 1
   instance_type = t2.micro #var.instance_type
-  vpc_security_group_ids = ["sg-23f2375c0"]
+  vpc_security_group_ids = ["sg-09a9a62ed86a291d3"]
   # subnet_id              =          "subnet-4eb39d02"
-  key_name   = "southed.pem" #lookup(var.keyname, var.region)
+  key_name   = "tempap.pem" #lookup(var.keyname, var.region)
   #monitoring = true
   #user_data              = file("D:/Terraform/Scripts/ubuntu.sh")
 
   tags = {
     Terraform   = "True"
-    Environment = "DeV"
+    Environment = "Test"
     Name        = "test_serv" #"SVR01PJTALPHA-A1530-${count.index}"
   }
 }
