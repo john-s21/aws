14857b2adc321c40e3cc2c9b6bd84466dfc3be19
JohnPaul <bijudonbosco1995@gmail.com>
2025-07-25 13:08:31 +0530

    Updated value for instanace_type & ami as string

--- master.tf
+++ master.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "TESTSERV001" {
-  ami           = ami-0e35ddab05955cf57 #lookup(var.ec2_ami, var.region)
+  ami           = "ami-0e35ddab05955cf57" #lookup(var.ec2_ami, var.region)
   count         = 1
-  instance_type = t2.micro #var.instance_type
+  instance_type = "t2.micro" #var.instance_type
   vpc_security_group_ids = ["sg-09a9a62ed86a291d3"]
   # subnet_id              =          "subnet-4eb39d02"
   key_name   = "tempap.pem" #lookup(var.keyname, var.region)
